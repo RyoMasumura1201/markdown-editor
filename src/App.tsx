9799d92058e519551d6a82c10308a1d5e2527ca2
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-08-11 18:44:17 +0900

    画面を二分割に

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "styles/app.module.scss";
 import { Textarea } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
+  const [note, setNote] = useState("");
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let inputValue = e.target.value;
+    setNote(inputValue);
+  };
 
   return (
     <div className="site-wrapper">
       <header></header>
       <main>
         <Grid templateColumns="repeat(2, 1fr)" gap={5} h="100vh">
-          <GridItem w="100%" h="100%" bg="blue.500">
-            <Textarea width="100%" height="100%" />
+          <GridItem w="100%" h="100%">
+            <Textarea
+              width="100%"
+              height="100%"
+              value={note}
+              onChange={handleInputChange}
+            />
           </GridItem>
-          <GridItem w="100%" h="100%" bg="blue.500" />
+          <GridItem w="100%" h="100%">
+            {note}
+          </GridItem>
         </Grid>
       </main>
     </div>
