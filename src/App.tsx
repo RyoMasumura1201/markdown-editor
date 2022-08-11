7d5655dbe2f3191e6a8c7a59a965165c47a68bca
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-08-11 18:35:39 +0900

    画面を二分割に

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,24 @@
-import { useState } from 'react'
-import electron from '/electron.png'
-import react from '/react.svg'
-import vite from '/vite.svg'
-import styles from 'styles/app.module.scss'
+import { useState } from "react";
+import styles from "styles/app.module.scss";
+import { Textarea } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <div className={styles.logos}>
-          <div className={styles.imgBox}>
-            <img
-              src={electron}
-              style={{ height: '24vw' }}
-              className={styles.appLogo}
-              alt="electron"
-            />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={vite} style={{ height: '19vw' }} alt="vite" />
-          </div>
-          <div className={styles.imgBox}>
-            <img
-              src={react}
-              style={{ maxWidth: '100%' }}
-              className={styles.appLogo}
-              alt="logo"
-            />
-          </div>
-        </div>
-        <p>Hello Electron + Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <div>
-          <a
-            className={styles.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className={styles.appLink}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          <div className={styles.staticPublic}>
-            Place static files into the{' '}
-            <code>/public</code> folder
-            <img style={{ width: 77 }} src="./node.png" />
-          </div>
-        </div>
-      </header>
+    <div className="site-wrapper">
+      <header></header>
+      <main>
+        <Grid templateColumns="repeat(2, 1fr)" gap={5} h="100vh">
+          <GridItem w="100%" h="100%" bg="blue.500">
+            <Textarea width="100%" height="100%" />
+          </GridItem>
+          <GridItem w="100%" h="100%" bg="blue.500" />
+        </Grid>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
